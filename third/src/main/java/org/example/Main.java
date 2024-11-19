10c6cdf1384fca6b11fc4fe7e4db802d8e45dcad
Sergei Klokov <s2584835@ya.ru>
2024-11-19 21:37:43 +0300

    homework6 final v2

--- third/src/main/java/org/example/Main.java
+++ third/src/main/java/org/example/Main.java
@@ -18,21 +18,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+    private final static int countGames = 1000;
     public static void main(String[] args) {
 
         Map<Integer, Boolean> changeChoose = new HashMap<>();
         Map<Integer, Boolean> notChangeChoose = new HashMap<>();
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < countGames; i++) {
             changeChoose.put(i, new PlayGame(3).play(true));
         }
-        for (int i = 0; i < 1000; i++) {
+        int countWinWithChange = (int)changeChoose.values().stream().filter(x -> x == true).count();
+        for (int i = 0; i < countGames; i++) {
             notChangeChoose.put(i, new PlayGame(3).play(false));
         }
+        int countWinWithNotChange = (int)notChangeChoose.values().stream().filter(x -> x == true).count();
 
+        System.out.println("Всего игр каждого типа по " + countGames);
         System.out.println("Кол-во побед, если изменить выбор двери = " +
-                changeChoose.values().stream().filter(x -> x == true).count());
+                countWinWithChange + ". Это " +
+                (int)(countWinWithChange * 1.0 / countGames * 100)  + "%");
         System.out.println("Кол-во побед, если НЕ менять выбор двери = " +
-                notChangeChoose.values().stream().filter(x -> x == true).count());
+                countWinWithNotChange + ". Это " +
+                (int)(countWinWithNotChange * 1.0 / countGames * 100)  + "%");
     }
 }
